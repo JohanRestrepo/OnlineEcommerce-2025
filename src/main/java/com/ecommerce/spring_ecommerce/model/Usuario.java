1e537b6fbadd4fdfa4e748536423a28bd795a321
Johan <esnic1994@gmail.com>
2025-11-13 19:45:51 -0500

    Modificamos los archivos de los modelos para ajustar la estructura de las tablas y que se crearan en la base de datos

--- src/main/java/com/ecommerce/spring_ecommerce/model/Usuario.java
+++ src/main/java/com/ecommerce/spring_ecommerce/model/Usuario.java
@@ -1,6 +1,14 @@
 package com.ecommerce.spring_ecommerce.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private String username;
@@ -10,10 +18,16 @@ public class Usuario {
     private String tipo;
     private String password;
 
+    @OneToMany(mappedBy = "usuario")
+    private List<Producto> productos;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Orden> ordenes;
+
     public Usuario() {
     }
 
-    public Usuario(Integer id, String nombre, String username, String mail, String direccion, String telefono, String tipo, String password) {
+    public Usuario(Integer id, String nombre, String username, String mail, String direccion, String telefono, String tipo, String password, List<Producto> productos, List<Orden> ordenes) {
         this.id = id;
         this.nombre = nombre;
         this.username = username;
@@ -22,6 +36,8 @@ public class Usuario {
         this.telefono = telefono;
         this.tipo = tipo;
         this.password = password;
+        this.productos = productos;
+        this.ordenes = ordenes;
     }
 
     public Integer getId() {
@@ -86,6 +102,14 @@ public class Usuario {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
     }
 
     @Override
