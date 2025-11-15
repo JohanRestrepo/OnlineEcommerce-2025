70f0698e7007581423948592bc5d82b073ff430f
Johan <esnic1994@gmail.com>
2025-11-15 13:36:29 -0500

    Se agrego la funcionalidad de guardar en el controlador y se ajusto el html para que se llame

--- src/main/java/com/ecommerce/spring_ecommerce/controller/ProductoController.java
+++ src/main/java/com/ecommerce/spring_ecommerce/controller/ProductoController.java
@@ -1,8 +1,13 @@
 package com.ecommerce.spring_ecommerce.controller;
 
+import com.ecommerce.spring_ecommerce.model.Producto;
+import org.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/productos")
@@ -16,5 +21,11 @@ public class ProductoController {
     @GetMapping("/create")
     public String create(){
         return "productos/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Producto producto){
+        System.out.println(producto);
+        return "redirect:/productos";
     }
 }
