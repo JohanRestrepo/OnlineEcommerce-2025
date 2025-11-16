54f56e2dbb89eeb2ed55a26ad9788097ccfb7154
Johan <esnic1994@gmail.com>
2025-11-16 18:14:27 -0500

    Se han agregado los productos al home, se agrego la carpeta d eimagenes como un recurso general, se arreglo un bug que eliminaba el id de los usuarios cuando se editaba un producto

--- src/main/java/com/ecommerce/spring_ecommerce/controller/ProductoController.java
+++ src/main/java/com/ecommerce/spring_ecommerce/controller/ProductoController.java
@@ -62,22 +62,21 @@ public class ProductoController {
 
     @PostMapping("/update")
     public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+
+        Producto p = new Producto();
+        p=productoService.get(producto.getId()).get();
+
         if(file.isEmpty()){//editamos un producto pero no cambiamos la imagen
-            Producto p = new Producto();
-            p=productoService.get(producto.getId()).get();
             producto.setImagen(p.getImagen());
         }else{//cuando se edita tambien la imagen
-
-            Producto p = new Producto();
-            p = productoService.get(producto.getId()).get();
             //eliminar la imagen siempre y cuando no sea la de por defecto
             if(!p.getImagen().equals("default.jpg")){
                 upload.deleteImage(p.getImagen());
             }
-
             String nombreImagen = upload.saveImage(file);
             producto.setImagen(nombreImagen);
         }
+        producto.setUsuario(p.getUsuario());
         productoService.update(producto);
         return "redirect:/productos";
     }
