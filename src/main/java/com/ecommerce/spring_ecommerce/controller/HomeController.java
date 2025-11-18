95e3a7e89231eb9aba5d6a2afa9190080a68ea8b
Johan <esnic1994@gmail.com>
2025-11-18 17:16:54 -0500

    Comenzamos la configuracion del carrito

--- src/main/java/com/ecommerce/spring_ecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/spring_ecommerce/controller/HomeController.java
@@ -1,15 +1,16 @@
 package com.ecommerce.spring_ecommerce.controller;
 
+import com.ecommerce.spring_ecommerce.model.DetalleOrden;
+import com.ecommerce.spring_ecommerce.model.Orden;
 import com.ecommerce.spring_ecommerce.model.Producto;
 import com.ecommerce.spring_ecommerce.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -18,6 +19,11 @@ public class HomeController {
 
     @Autowired
     private ProductoService productoService;
+    //Para almacenar los detalles de la orden
+    List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
+
+    //Datos de la orden
+    Orden orden = new Orden();
 
     @GetMapping("")
     public String home(Model model){
@@ -35,7 +41,14 @@ public class HomeController {
     }
 
     @PostMapping("/cart")
-    public String addCart(){
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad){
+        DetalleOrden detalleOrden = new DetalleOrden();
+        Producto producto = new Producto();
+        double sumaTotal = 0;
+
+        Optional<Producto> optionalProducto = productoService.get(id);
+        System.out.println("Producto añadido" + optionalProducto);
+        System.out.println("Cantidad: " + cantidad);
         return "usuario/carrito";
     }
 }
