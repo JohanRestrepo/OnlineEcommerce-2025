bc7e00a98f84347f2aa58908c062a023f3cdaf6c
Johan <esnic1994@gmail.com>
2025-11-20 14:03:05 -0500

    Agregamos la creacion del numero de la orden

--- src/main/java/com/ecommerce/spring_ecommerce/service/IOrdenService.java
+++ src/main/java/com/ecommerce/spring_ecommerce/service/IOrdenService.java
@@ -2,6 +2,10 @@ package com.ecommerce.spring_ecommerce.service;
 
 import com.ecommerce.spring_ecommerce.model.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
+    List<Orden> findAll();
     Orden save(Orden orden);
+    String generarNumeroOrden();
 }
