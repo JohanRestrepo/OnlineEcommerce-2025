7ccfa93e3d2f9e44060bfbcc6da8df69d0058c28
Johan <esnic1994@gmail.com>
2025-11-21 19:17:52 -0500

    Agregamos la nueva pagina para registro de usuarios y el cotnrolador asociado

---
+++ src/main/java/com/ecommerce/spring_ecommerce/controller/UsuarioController.java
@@ -0,0 +1,20 @@
+package com.ecommerce.spring_ecommerce.controller;
+
+import com.ecommerce.spring_ecommerce.service.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioServiceImpl usuarioService;
+
+    @GetMapping("/registro")
+    public String registro(){
+        return "usuario/registro";
+    }
+}
