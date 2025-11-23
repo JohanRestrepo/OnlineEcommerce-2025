7e84db45cf4c1f5b941ba62cda54abbca07ced61
Johan <esnic1994@gmail.com>
2025-11-23 14:10:15 -0500

    Agregamos la pantalla para ver las compras del usuario logueado

--- src/main/java/com/ecommerce/spring_ecommerce/controller/UsuarioController.java
+++ src/main/java/com/ecommerce/spring_ecommerce/controller/UsuarioController.java
@@ -4,7 +4,9 @@ import com.ecommerce.spring_ecommerce.model.Usuario;
 import com.ecommerce.spring_ecommerce.service.UsuarioServiceImpl;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,5 +53,11 @@ public class UsuarioController {
             System.out.println("usuario no existe");
         }
         return "redirect:/";
+    }
+
+    @GetMapping("/compras")
+    public String obtenerCompras(HttpSession session, Model model){
+        model.addAttribute("sesion", session.getAttribute("idusuario"));
+        return "usuario/compras";
     }
 }
